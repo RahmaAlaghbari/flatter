b1f01f7b6f6efc230f78667e0d9808cd36c94be3
Ra7ma00 <140978634+Ra7ma00@users.noreply.github.com>
2023-10-18 15:23:15 +0300

    wednesday

--- project4/lib/MainPages/login_page.dart
+++ project4/lib/MainPages/login_page.dart
@@ -1,216 +1,247 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'Home.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>(); // GlobalKey for the form
-
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  void _login() async {
-    // Create an instance of Dio
-    final dio = Dio();
-
-    try {
-      // Send a POST request to your login API endpoint
-      final response = await dio.post(
-        'https://your-api-endpoint.com/login',
-        data: {
-          'email': _emailController.text,
-          'password': _passwordController.text,
-        },
-      );
-
-      // Check the response status code
-      if (response.statusCode == 200) {
-        // Login successful, navigate to the home page
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => FooterBar()),
-        );
-      } else {
-        // Login failed, display an error message
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text('Login Failed'),
-            content: Text('Invalid email or password.'),
-            actions: [
-              TextButton(
-                child: Text('OK'),
-                onPressed: () => Navigator.pop(context),
-              ),
-            ],
-          ),
-        );
-      }
-    } catch (error) {
-      // Handle any errors that occurred during the request
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Error'),
-          content: Text('An error occurred. Please try again later.'),
-          actions: [
-            TextButton(
-              child: Text('OK'),
-              onPressed: () => Navigator.pop(context),
-            ),
-          ],
-        ),
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    // Clean up the text controllers
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+  bool _isSubmitPressed = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(30.0),
-        color: Colors.grey[200],
-        child: Form(
-          key: _formKey, // Assign the GlobalKey to the form
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-            Text(
-            'Login',
-            style: TextStyle(
-              fontSize: 32.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueGrey,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(height: 60.0),
-          TextFormField(
-            controller: _emailController,
-            decoration: InputDecoration(
-              labelText: 'Email',
-              prefixIcon: Icon(Icons.email),
-              filled: true,
-              fillColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: BorderSide(
-                  color: Colors.blueGrey,
-                ),
-              ),
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your email';
-              }
-              if (value.length < 5) {
-                return 'Email must be more than 5 characters';
-              }
-              if (!value.contains('@')) {
-                return 'Email must include @';
-              }
-              return null;
-            },
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            controller: _passwordController,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              prefixIcon: Icon(Icons.lock),
-              filled: true,
-              fillColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: BorderSide(
-                  color: Colors.blueGrey,
-                ),
-              ),
-            ),
-            obscureText: true,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your password';
-              }
-              if (value.trim().isEmpty) {
-                return "Don't make space";
-              }
-              return null;
-            },
-          ),
-          SizedBox(height: 40.0),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                // Validation successful, perform login action
-                _login();
-              }
-            },
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blueGrey,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-            ),
-            child: Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            TextButton(
-              onPressed: () {
-                // Navigate to registration page
-              },
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-              ),
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Center(
               child: Text(
-                'Create an account',
+                'Ninja-In',
                 style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.blueGrey,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 4,
                 ),
               ),
             ),
-            SizedBox(height: 0.0),
-            TextButton(
-              onPressed: () {
-                // Navigate to forgot password page
-              },
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-              ),
-              child: Text(
-                'Forgot your password?',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.blueGrey,
-                ),
-              ),
-            ),
-          )
-            ],
           ),
-        ),
+          Form(
+            key: formKey,
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    autovalidateMode: _isSubmitPressed
+                        ? AutovalidateMode.disabled
+                        : AutovalidateMode.onUserInteraction,
+                    controller: _emailController,
+                    validator: (value) {
+                      RegExp emailRegex =
+                      RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+                      if (value == null) {
+                        return 'email is required';
+                      } else if (value.length < 10) {
+                        return 'email must be more than 10 char';
+                      } else if (!value.contains('@')) {
+                        return 'email must contain @';
+                      } else if (!emailRegex.hasMatch(value)) {
+                        return 'invalid email';
+                      }
+                      return null;
+                    },
+                    maxLength: 30,
+                    decoration: InputDecoration(
+                      labelText: 'E-mail',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Theme.of(context).primaryColorLight,
+                        ),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.redAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: TextFormField(
+                    obscureText: true,
+                    controller: _passwordController,
+                    autovalidateMode: _isSubmitPressed
+                        ? AutovalidateMode.disabled
+                        : AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value == null) {
+                        return 'password is required';
+                      } else if (value.length < 3) {
+                        return 'password must be more than 3 char';
+                      } else {
+                        return null;
+                      }
+                    },
+                    maxLength: 20,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Theme.of(context).primaryColorLight,
+                        ),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: _isLoading
+                      ? Visibility(
+                    visible: _isLoading,
+                    child: CircularProgressIndicator(),
+                  )
+                      : TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Theme.of(context).primaryColorDark,
+                      backgroundColor: Theme.of(context).primaryColor,
+                      minimumSize: Size(
+                        MediaQuery.of(context).size.width,
+                        50,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (formKey.currentState!.validate()) {
+                        setState(() {
+                          _isLoading = true;
+                          _isSubmitPressed = true;
+                        });
+
+
+
+
+                        final String loginUrl = 'https://65253db067cfb1e59ce6f039.mockapi.io/hotelusers/users';
+
+                        try {
+                          Dio dio = Dio();
+                          final response = await dio.get(
+                            loginUrl,
+                            data: {
+                              'email': _emailController.text,
+                              'password': _passwordController.text,
+                            },
+                          );
+
+                          if (response.statusCode == 200) {
+                            
+                            // Login successful
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => FooterBar()),
+                            );
+                          } else {
+                            // Login failed
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("Invalid email or password"),
+                                backgroundColor: Colors.redAccent,
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                          }
+                        } catch (e) {
+                          // Error occurred during login API call
+                          print('Login API error: $e');
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("An error occurred. Please try again."),
+                              backgroundColor: Colors.redAccent,
+                              duration: Duration(seconds: 2),
+                            ),
+                          );
+                        }
+                        // Perform login API call here
+                        // Replace the code below with your actual login API call
+                        // UserModel apiUser = await userRepo.login(user);
+                        // if (apiUser.id != null) {
+                        //   Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(builder: (context) => FooterBar()),
+                        //   );
+                        // } else {
+                        //   ScaffoldMessenger.of(context).showSnackBar(
+                        //     SnackBar(
+                        //       content: Text("Invalid email or password"),
+                        //       backgroundColor: Colors.redAccent,
+                        //       duration: Duration(seconds: 2),
+                        //     ),
+                        //   );
+                        // }
+
+                        // Simulating a successful login for testing purposes
+                        await Future.delayed(Duration(seconds: 2));
+
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => FooterBar()),
+                        );
+
+                        setState(() {
+                          _isLoading = false;
+                          _isSubmitPressed = true;
+                        });
+                      }
+                    },
+                    child: Text('Submit'),
+                  ),
+                ),
+                const Text(
+                  'Don\'t have an account?',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                TextButton(
+                  child: const Text(
+                    'Sign up',
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 15,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/signup');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- project4/lib/main.dart
+++ project4/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: SignUpPage(),
+      home: LoginPage(),
       //home: ProfilePage(),
       //home: FooterBar(),
       //home:HotelColumn(),
